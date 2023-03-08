c8792b1bb8c6507de82220018f469c307a1d8a18
lazarusmugo <lazzohchainz@gmail.com>
2023-03-08 16:03:33 +0300

    changes to admin

--- admin_files/admin_dashboard.php
+++ admin_files/admin_dashboard.php
@@ -1,19 +1,20 @@
+<?php
+session_start(); // start the session
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Admin Dashboard</title>
 </head>
 <body>
-
-<h1>Welcome, <?php echo $_SESSION['admin_username']; ?>!</h1>
-
-<ul>
-  <li><a href="manage_voters.php">Manage Voters</a></li>
-  <li><a href="manage_positions.php">Manage Positions</a></li>
-  <li><a href="manage_candidates.php">Manage Candidates</a></li>
-  <li><a href="view_results.php">View Results</a></li>
-  <li><a href="logout.php">Logout</a></li>
-</ul>
-
+  <h1>Welcome To The Admin Dashboard</h1>
+  <ul>
+    <li><a href="manage_voters.php">Manage Voters</a></li>
+    <li><a href="manage_positions.php">Manage Positions</a></li>
+    <li><a href="manage_candidates.php">Manage Candidates</a></li>
+    <li><a href="view_results.php">View Results</a></li>
+    <li><a href="admin_logout.php">Logout</a></li>
+  </ul>
 </body>
 </html>
