c62bb55c26e21e2b7436148753a4b7c36eb574bb
lazarusmugo <lazzohchainz@gmail.com>
2023-03-08 13:51:35 +0300

    fixed multiple voting

--- vote.php
+++ vote.php
@@ -1,71 +1,80 @@
 <?php
 include("connection.php");
 
-// Check if the user is logged in and hasn't voted yet
+// Check if the user is logged in
 session_start();
-if(!isset($_SESSION['ID']) || $_SESSION['voted'] == 1){
-    header("Location: index.php");
+if(!isset($_SESSION['ID'])){
+    header("Location: login.php");
     exit();
 }
 
-// If the form is submitted, update the candidate's vote count and mark the user as having voted
-if(isset($_POST['submit_vote'])){
-    $voter_id = $_POST['voter_id'];
-    
-    // Loop through each position
-    $sql_positions = "SELECT * FROM positions";
-    $result_positions = mysqli_query($conn, $sql_positions);
-    while($row_positions = mysqli_fetch_assoc($result_positions)){
-        $position_id = $row_positions['Position ID'];
-        $candidate_id = $_POST["position-$position_id"];
-        
-        // Update the candidate's vote count in the database
-        $sql_update = "UPDATE candidates SET `number of votes` = `number of votes` + 1 WHERE `candidate ID` = '$candidate_id'";
-        mysqli_query($conn, $sql_update);
-    }
-    
-    // Mark the user as having voted in the database
-    $sql_mark_voted = "UPDATE voters SET voted = 1 WHERE ID = '$voter_id'";
-    mysqli_query($conn, $sql_mark_voted);
-    
-    // Redirect the user back to the home page
-    header("Location: home_page.php");
+// Check if the user has already voted
+$voter_id = $_SESSION['ID'];
+$sql_check_voted = "SELECT Voted FROM voters WHERE ID = '$voter_id'";
+$result_check_voted = mysqli_query($conn, $sql_check_voted);
+$row_check_voted = mysqli_fetch_assoc($result_check_voted);
+if($row_check_voted['Voted'] == 1){
+    echo "You had already voted! Your vote has not been counted";
+    //header("Location: index.php");
     exit();
 }
 
-// If the user is allowed to vote, display the list of positions and candidates
+// Display the list of positions and candidates
 $sql_positions = "SELECT * FROM positions";
 $result_positions = mysqli_query($conn, $sql_positions);
 
 if(mysqli_num_rows($result_positions) > 0) {
+    echo "<form method='post'>";
     while($row_positions = mysqli_fetch_assoc($result_positions)){
-        $position_id = $row_positions['PositionID'];
+        $position_id = $row_positions['Position ID'];
         $position_name = $row_positions['Name'];
 
         // Display the position name
         echo "<h3>$position_name</h3>";
 
         // Get the candidates for this position from the database
-        $sql_candidates = "SELECT * FROM candidates WHERE position ID = '$position_id'";
+        $sql_candidates = "SELECT * FROM candidates WHERE 'position ID' = '$position_id'";
         $result_candidates = mysqli_query($conn, $sql_candidates);
 
         // Display a form to allow the user to vote for a candidate
-        echo "<form method='post'>";
         while($row_candidates = mysqli_fetch_assoc($result_candidates)){
-            $candidate_id = $row_candidates['CandidateID'];
+            $candidate_id = $row_candidates['Candidate ID'];
             $candidate_name = $row_candidates['Name'];
-            $num_of_votes = $row_candidates['num_of_votes'];
 
-            echo "<label>
-                    <input type='radio' name='position-$position_id' value='$candidate_id' required>
-                    $candidate_name ($num_of_votes votes)
-                  </label><br>";
+            echo "<input type='radio' name='position-$position_id' value='$candidate_id'> $candidate_name<br>";
         }
-        echo "<br>";
     }
+    echo "<input type='hidden' name='voter_id' value='$voter_id'>";
+    echo "<input type='submit' name='submit_vote' value='Vote'>";
+    echo "</form>";
 }
-echo "<input type='hidden' name='voter_id' value='".$_SESSION['ID']."'>";
-echo "<input type='submit' name='submit_vote' value='Submit Vote'>";
-echo "</form>";
 
+// If the form is submitted, update the candidate's vote count and mark the user as having voted
+if(isset($_POST['submit_vote'])){
+    // Check if the user has already voted
+    $voter_id = $_POST['voter_id'];
+    $sql_check_voted = "SELECT Voted FROM voters WHERE ID = '$voter_id'";
+    $result_check_voted = mysqli_query($conn, $sql_check_voted);
+    $row_check_voted = mysqli_fetch_assoc($result_check_voted);
+    if($row_check_voted['Voted'] == 1){
+    echo "You have already voted!";
+    } else {
+    // Loop through the submitted form data to update the vote count for each candidate
+    foreach($_POST as $key => $value){
+    if(substr($key, 0, 9) == "position-"){
+    $position_id = substr($key, 9);
+    $candidate_id = $value;
+             // Update the vote count for the candidate
+             $sql_update_votes = "UPDATE candidates SET `number of votes` = `number of votes` + 1 WHERE `Candidate ID` = '$candidate_id'";
+             mysqli_query($conn, $sql_update_votes);
+         }
+     }
+ 
+     // Mark the user as having voted
+     $sql_mark_voted = "UPDATE voters SET Voted = 1 WHERE ID = '$voter_id'";
+     mysqli_query($conn, $sql_mark_voted);
+ 
+     echo "Thank you for voting!";
+ }
+}
 ?>
